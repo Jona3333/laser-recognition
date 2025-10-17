ebce43573d542a490703495c448e03e82e532ddc
Jona3333 <jonathan.etin13@gmail.com>
2025-10-17 08:46:25 +0300

    more code

--- arduino_image_capture/arduino_image_capture.ino
+++ arduino_image_capture/arduino_image_capture.ino
@@ -1,52 +1,28 @@
-#include <Wire.h>
-#include <SPI.h>
-#include <ArduCAM.h>
-#include "memorysaver.h"
-
-#define CS_PIN 7
-
-ArduCAM myCAM(OV2640, CS_PIN);
+// Arduino Serial-controlled LED example
+const int LED_PIN = 13;
+String line = "";
 
 void setup() {
-  Wire.begin();
-  Serial.begin(115200);
-  SPI.begin();
-
-  pinMode(CS_PIN, OUTPUT);
-  myCAM.write_reg(ARDUCHIP_TEST1, 0x55);
-  if (myCAM.read_reg(ARDUCHIP_TEST1) != 0x55) {
-    Serial.println("Camera not found!");
-    while (1);
-  }
-
-  myCAM.set_format(JPEG);
-  myCAM.InitCAM();
-  myCAM.OV2640_set_JPEG_size(OV2640_640x480);
-  delay(1000);
-  Serial.println("READY");
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  Serial.begin(115200); // make sure Python uses the same baud
 }
 
 void loop() {
-  myCAM.flush_fifo();
-  myCAM.clear_fifo_flag();
-  myCAM.start_capture();
-  Serial.println("CAPTURING...");
-  while (!myCAM.get_bit(ARDUCHIP_TRIG, CAP_DONE_MASK));
-
-  uint32_t length = myCAM.read_fifo_length();
-  Serial.print("SIZE:");
-  Serial.println(length);
-
-  myCAM.CS_LOW();
-  myCAM.set_fifo_burst();
-  const uint8_t BURST_FIFO_READ = 0x3C;
-  SPI.transfer(BURST_FIFO_READ);
-  
-  while (length--) {
-    uint8_t b = SPI.transfer(0x00);
-    Serial.write(b); // send raw bytes to PC
+  while (Serial.available()) {
+    char c = Serial.read();
+    if (c == '\n') {
+      line.trim();
+      if (line == "LASER ON") digitalWrite(LED_PIN, HIGH);
+      else if (line == "LASER OFF") digitalWrite(LED_PIN, LOW);
+      else if (line.startsWith("PWM ")) {
+        int val = line.substring(4).toInt(); // 0-255
+        analogWrite(9, constrain(val,0,255)); // example PWM on pin 9
+      }
+      Serial.println("OK"); // simple acknowledgement
+      line = "";
+    } else {
+      line += c;
+    }
   }
-  myCAM.CS_HIGH();
-  Serial.println("DONE");
-  delay(10000);
 }
