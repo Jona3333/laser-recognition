2b38ee8de23ad520c07b1fcc3258e0e02c527d4b
ohad-alon47 <ohad.alon.47@gmail.com>
2025-10-17 10:28:52 +0300

    new dist formula

--- Params.py
+++ Params.py
@@ -3,8 +3,31 @@ import math
 class Params:
     PIXELS = 1920
     d = 0.06 # in cm
+
     ALPHA = math.radians(26.91)
-    tan = math.tan(ALPHA)
-    a = (PIXELS * d) / (2*tan)
-    correction = 1
-    A = a * correction
+    tan_ALPHA = math.tan(ALPHA)
+
+    BETA1 = math.radians()
+    BETA2 = math.radians()
+    BETA3 = math.radians()
+    BETA4 = math.radians()
+
+    tan_BETA1 = math.tan(BETA1)
+    tan_BETA2 = math.tan(BETA2)
+    tan_BETA3 = math.tan(BETA3)
+    tan_BETA4 = math.tan(BETA4)
+
+    a_correction = 1
+    b_correction = 1
+
+    camera_diff = 0.04 # in cm
+
+def sigma_tan(duo):
+    match duo:
+        case 1:
+            return (Params.tan_BETA1 + Params.tan_BETA2)
+        case 2:
+            return (Params.tan_BETA2 + Params.tan_BETA3)
+        case 3:
+            return (Params.tan_BETA3 + Params.tan_BETA4)
+    return 0
--- return_dist.py
+++ return_dist.py
@@ -1,14 +1,16 @@
-import Params.Params as par
+import Params.Params as p
 import math
 
-'''
 def pixels_dist(x1,y1,x2,y2):
-    square = (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)
-    return math.sqrt(square)
-'''
-
-def pixels_dist(x1, y1, x2, y2):
     return abs(x1-x2)
 
-def dist(pixels):
-    return par.A * 1/pixels
+def find_dist(pixels, duo):
+    return (p.PIXELS * p.d) / (2*p.tan_ALPHA*pixels - p.PIXELS*p.sigma_tan(duo))
+
+def dist(points):
+    dists = [0,0,0]
+    for i in range(3):
+        x1,y1=points[i]
+        x2,x2=points[i+1]
+        pixels=pixels_dist(x1,y1,x2,y2)
+        dists[i] = find_dist(pixels,i) - p.camera_diff
