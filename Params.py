2c8be9870ec2e91ca8c35223529d441e5cd7c7a7
ohad-alon47 <ohad.alon.47@gmail.com>
2025-10-17 08:48:06 +0300

    better Params

--- Params.py
+++ Params.py
@@ -2,7 +2,9 @@ import math
 
 class Params:
     PIXELS = 1920
-    d = 0.06
+    d = 0.06 # in cm
     ALPHA = math.radians(26.91)
     tan = math.tan(ALPHA)
-    A = (PIXELS * d) / (2*tan)
+    a = (PIXELS * d) / (2*tan)
+    correction = 1
+    A = a * correction
