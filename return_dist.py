ef38361a515a337c73482ce51e92de4c37c25162
ohad-alon47 <ohad.alon.47@gmail.com>
2025-10-17 08:15:13 +0300

    add pixel_dist (both options)

--- return_dist.py
+++ return_dist.py
@@ -1,2 +1,14 @@
+import Params.Params as par
+import math
+
+'''
+def pixels_dist(x1,y1,x2,y2):
+    square = (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)
+    return math.sqrt(square)
+'''
+
+def pixels_dist(x1, y1, x2, y2):
+    return abs(x1-x2)
+
 def dist(pixels):
-    return A * 1/pixels
+    return par.A * 1/pixels
